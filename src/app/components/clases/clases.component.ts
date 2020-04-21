9ee583b8f8efc1f88fe883237cb32dc5db7664fc
Jose Illan <jose_illan@hotmail.com>
2020-04-22 00:17:23 +0200

    Fix error in type of object

---
+++ src/app/components/clases/clases.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+
+  alerta:string = "alert-danger"
+  aaa= true
+
+  propiedades:any = {
+    danger: true
+  }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
